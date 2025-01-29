2f3bf184c5b7403367bcc249cf24ad4d32eb0a3f
pspeasegood-ch <ppeasegood@companieshouse.gov.uk>
2025-01-29 14:53:18 +0000

    Set up Project Structure

---
+++ src/main/java/uk/gov/companieshouse/postcodelookup/PostcodeLookApplication.java
@@ -0,0 +1,12 @@
+package uk.gov.companieshouse.postcodelookup;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcodeLookApplication {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
